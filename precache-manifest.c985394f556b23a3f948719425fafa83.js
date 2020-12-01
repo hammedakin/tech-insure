3341eaca895708f9eb298dfcb0aca9613d12253f
omoluabii <hakra911@gmail.com>
2020-12-01 16:45:52 +0100

    Updates

---
+++ precache-manifest.c985394f556b23a3f948719425fafa83.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50139ee09ebad5708f021b170806e8ed",
+    "url": "/tech-insure/index.html"
+  },
+  {
+    "revision": "ed4b91665b86f9b0ab75",
+    "url": "/tech-insure/static/css/main.d5a022f2.chunk.css"
+  },
+  {
+    "revision": "43d804d8bee6d97cd483",
+    "url": "/tech-insure/static/js/2.9c207449.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tech-insure/static/js/2.9c207449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed4b91665b86f9b0ab75",
+    "url": "/tech-insure/static/js/main.52610fe3.chunk.js"
+  },
+  {
+    "revision": "1b0cc28faceba8420226",
+    "url": "/tech-insure/static/js/runtime-main.c1cbb241.js"
+  },
+  {
+    "revision": "453b8831f7990d52fe3fa771346332b0",
+    "url": "/tech-insure/static/media/company3.453b8831.png"
+  },
+  {
+    "revision": "462563f84076f23ba22fff24690dd030",
+    "url": "/tech-insure/static/media/company4.462563f8.jpg"
+  },
+  {
+    "revision": "c9713c1df33836cb30dd313aec884900",
+    "url": "/tech-insure/static/media/footer1.c9713c1d.jpg"
+  },
+  {
+    "revision": "6cc71f6c794747892792ec613ad5ddb7",
+    "url": "/tech-insure/static/media/footer2.6cc71f6c.jpg"
+  },
+  {
+    "revision": "55a9fab244ed4b19f984fe83ad841318",
+    "url": "/tech-insure/static/media/footer3.55a9fab2.jpg"
+  },
+  {
+    "revision": "91fba5f083c059d86904e05652334481",
+    "url": "/tech-insure/static/media/landing.91fba5f0.png"
+  },
+  {
+    "revision": "5cd660022e310347ea2a446fe79a4468",
+    "url": "/tech-insure/static/media/services1.5cd66002.png"
+  },
+  {
+    "revision": "91fba5f083c059d86904e05652334481",
+    "url": "/tech-insure/static/media/services2.91fba5f0.png"
+  },
+  {
+    "revision": "edda3caa9b3759d75de66ff759e8fef4",
+    "url": "/tech-insure/static/media/services3.edda3caa.png"
+  },
+  {
+    "revision": "748110b05e5588ee617b7c97f939efac",
+    "url": "/tech-insure/static/media/services4.748110b0.png"
+  }
+]);
